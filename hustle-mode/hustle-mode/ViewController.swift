cf2338555c3740160dd32f70ffc4eb35e3523127
Eliza Riviera <eliza.riviera@kudo.co.id>
2018-05-01 17:51:40 +0700

    init hustle app

---
+++ hustle-mode/hustle-mode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Eliza Riviera on 01/05/18.
+//  Copyright © 2018 Eliza Riviera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var darkBlueBg: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolderView: UIView!
+    @IBOutlet weak var rocketView: UIImageView!
+    @IBOutlet weak var hustleModeLbl: UILabel!
+    @IBOutlet weak var OnLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
